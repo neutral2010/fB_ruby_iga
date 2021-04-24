7afa73b3afdb6594e73a0e96f267fb217d4754b3
neutral2010 <kasiwamoti5@gmail.com>
2021-04-24 09:00:39 +0900

    一旦、commit

--- ch_8/1.rb
+++ ch_8/1.rb
@@ -1,3 +1,8 @@
 puts "問1"
 
 p ({ :coffee => 300, :caffe_latte => 400}).class
+
+
+puts "問2"
+hash = Hash.new()
+p hash
---
+++ ch_8/3.rb
@@ -0,0 +1,9 @@
+class Item
+  def name
+    return "チーズケーキ"
+  end
+end
+
+item = Item.new
+t
+puts name()
